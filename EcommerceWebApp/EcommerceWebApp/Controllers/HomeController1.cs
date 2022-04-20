4fb9f77deff7333ece57b022e30bd52d485264c7
maheshpaladugu <maheshpaladugu9@gmail.com>
2022-04-20 17:27:32 +0530

    sp-2

---
+++ EcommerceWebApp/EcommerceWebApp/Controllers/HomeController1.cs
@@ -0,0 +1,27 @@
+﻿using EcommerceWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceWebApp.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        EcommerceDbContext _ecommerceDbContext;
+        public HomeController1(EcommerceDbContext ecommerceDbContext)
+        {
+            _ecommerceDbContext = ecommerceDbContext;
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IEnumerable<TblProduct> GetCategories()
+        {
+            return _ecommerceDbContext.TblProducts.ToList();
+        }
+    }
+}
